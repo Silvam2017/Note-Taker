978e7b798b5f98856613a1360ab5c0d8b91b6b5f
Michael Silva <msilva@aggienetwork.com>
2020-05-31 12:32:49 -0500

    added server.js file.

---
+++ Develop/server.js
@@ -0,0 +1,25 @@
+const api = require('./post/notes');
+
+const html = require('./post/html');
+
+const express = require('express');
+
+const fs = require('fs');
+
+const path = ('path');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.use(express.urlencoded({extended: true }));
+
+app.use('/api', api);
+
+app.use('/', html);
+
+app.listen(PORT, () => {
+    console.log(`Server is now active on port ${PORT}.`);
+});
